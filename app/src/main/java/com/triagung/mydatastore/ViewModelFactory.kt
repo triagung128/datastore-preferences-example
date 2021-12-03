7de08d274ef271cf070c7a3de96071b42507099e
triagung128 <triagung128@gmail.com>
2021-12-04 05:55:20 +0700

    first commit

---
+++ app/src/main/java/com/triagung/mydatastore/ViewModelFactory.kt
@@ -0,0 +1,16 @@
+package com.triagung.mydatastore
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider.NewInstanceFactory
+import java.lang.IllegalArgumentException
+
+class ViewModelFactory(private val pref: SettingPreferences) : NewInstanceFactory() {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            return MainViewModel(pref) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
+    }
+}
